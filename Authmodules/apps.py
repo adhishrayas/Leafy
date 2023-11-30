97bd25cd9ae6b8415bf2868c449ac2cc08c5f041
adhishrayas <adhishrayas@gmail.com>
2023-11-30 18:23:19 +0530

    again debugged mails

--- Authmodules/apps.py
+++ Authmodules/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class AuthmodulesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Authmodules'
-
-    def ready(self):
-        from helpers import mailings
